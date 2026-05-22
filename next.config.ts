027cbdf757117a4a36a6a0108a4f85fd314615db
patedde <122355697+patedde@users.noreply.github.com>
2026-05-22 14:41:05 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true
 };
 
 export default nextConfig;
